72dd1f53a448f212046db3a1c4241b26a9ad4983
Natasha Ann Lum <natashaannlum@gmail.com>
2024-08-16 15:08:04 +0800

    chore: update next.config.mjs
    Change config for next.js to build static assets for serving on GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    basePath: "/ragtech-website",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
 export default nextConfig;
